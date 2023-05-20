94946fad0c3035f2e02c14f3230b5942cf0f0fbb
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2023-05-20 22:17:39 +0800

    camera followed the player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 tempPosition;
+
+    [SerializeField]
+    private int minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void LateUpdate()
+    {
+        tempPosition = transform.position;
+        tempPosition.x = player.position.x;
+
+        if (tempPosition.x < minX)
+            tempPosition.x = minX;
+        if (tempPosition.x > maxX)
+            tempPosition.x = maxX;
+
+        transform.position = tempPosition;
+    }
+}
