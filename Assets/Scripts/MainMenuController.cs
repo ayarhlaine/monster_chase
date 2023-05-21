7d712a444c2e23410935055e921b0fcf0f6d1035
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2023-05-21 13:32:47 +0800

    spawned character based on selected char

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
    public void PlayGame()
     {
-        Debug.Log("Button is pressed!");
+        int selectedIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+        GameManager.instance.CharIndex = selectedIndex;
+
+        SceneManager.LoadScene("Gameplay");
     }
 }
